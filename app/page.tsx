7b1b5c4671c783d8f4552483f968104330f46a72
Hyun <87049207+vaynel@users.noreply.github.com>
2024-12-25 21:31:18 +0900

    first page changed

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import React, { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { isAuthenticated } from '@/utils/auth';
+import styled from 'styled-components';
 
 const HomePage = () => {
   const router = useRouter();
@@ -16,10 +17,26 @@ const HomePage = () => {
   }, [router]);
 
   return (
-    <div>
-      <p>로그인 상태를 확인 중입니다...</p>
-    </div>
+    <Container>
+      <Message>로그인 상태를 확인 중입니다...</Message>
+    </Container>
   );
 };
 
 export default HomePage;
+
+// Styled Components
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh; /* 전체 화면 높이 */
+  background-color: #f8f9fa; /* 부드러운 배경색 */
+`;
+
+const Message = styled.p`
+  font-size: 18px;
+  font-weight: bold;
+  color: #007bff; /* 파란색 텍스트 */
+  text-align: center;
+`;
